ef893490214352e352bbf41f631354a6241c3c29
Luciano <lucianorodriguess101@gmail.com>
2025-02-18 22:00:50 -0300

    feat(api): update play API endpoint to handle JSON body and add authorization headers

--- src/app/api/play/route.ts
+++ src/app/api/play/route.ts
@@ -5,11 +5,15 @@ export async function POST(request: Request) {
   const apiUrl = `${process.env.API_URL}/play`;
 
   try {
-    const response = await axios.post(apiUrl, request.body);
+    const body = await request.json();
+    const response = await axios.post(apiUrl, body, {
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTA0MGUwZTQtZTYxZS00NjI5LWI4YWItZTVkNzc2NWIzMGZjIiwidG9rZW5fdHlwZSI6ImFjY2VzcyIsImV4cCI6MTczOTkyNTIzOSwiaWF0IjoxNzM5OTI0MzM5fQ.WxXinMltjha2s2_rh8Wy84-pPh1k1rhhavLTY-wIOMc`,
+        "Content-Type": "application/json",
+      },
+    });
     return NextResponse.json(response.data, { status: 200 });
   } catch (error) {
-    console.log(error);
-
     console.error("Erro na API interna:", error);
     return NextResponse.json(
       { error: "Erro ao processar a solicitação" },
