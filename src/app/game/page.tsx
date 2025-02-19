4c4e448d4a6ccdb24b36246e04ce258de5eb03fb
Luciano <lucianorodriguess101@gmail.com>
2025-02-18 22:00:41 -0300

    feat(pages): add Game page with SlotMachine component

---
+++ src/app/game/page.tsx
@@ -0,0 +1,16 @@
+import { SlotMachine } from "@/components/SlotMachine";
+
+export default function Game() {
+  return (
+    <div>
+      <SlotMachine />
+
+      <p>
+        <span role="img" aria-label="code">
+          {/* pelo code */}
+          &#x1F911;
+        </span>
+      </p>
+    </div>
+  );
+}
