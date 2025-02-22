5f805039ad973a6ccd7ff9d6317870ebe885b8b1
Luciano <lucianorodriguess101@gmail.com>
2025-02-22 16:00:17 -0300

    feat(button): add ButtonProps interface and variant classes for button styling

---
+++ src/components/button/button-props.ts
@@ -0,0 +1,13 @@
+type Variant = "primary" | "secondary";
+
+export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
+  variant?: Variant;
+}
+
+export type VariantClasses = Record<Variant, string>;
+
+export const variantClasses: VariantClasses = {
+  primary: "bg-primary text-black",
+  secondary:
+    "bg-backgound text-white transition-colors hover:bg-accent hover:text-black",
+};
