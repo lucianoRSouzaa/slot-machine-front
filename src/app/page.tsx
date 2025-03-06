bca85105faaad84f2b04ab983818ddc6b03c8895
Luciano <lucianorodriguess101@gmail.com>
2025-03-05 22:03:22 -0300

    feat(page): integrate Header component into Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 import { SlotMachine } from "@/components/slot-machine/SlotMachine";
 import "./globals.css";
 import { Sidebar } from "@/components/sidebar/Sidebar";
+import { Header } from "@/components/header/Header";
 
 export default function Home() {
   return (
     <div className="flex">
       <Sidebar />
 
-      <div className="flex flex-1 justify-center items-center">
-        <SlotMachine />
-      </div>
+      <div className="flex-1">
+        <Header />
 
-      {/* exibir emoji de code: 1F911 */}
-      <p>
+        <div className="flex flex-1 justify-center items-center">
+          <SlotMachine />
+        </div>
+
+        {/* exibir emoji de code: 1F911 */}
+        {/* <p>
         <span role="img" aria-label="code">
-          {/* pelo code */}
           &#x1F911;
         </span>
-      </p>
+      </p> */}
+      </div>
     </div>
   );
 }
